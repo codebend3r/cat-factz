3cd959f1cca9c07ba584a744597f7b78cc4e0806
Chester Rivas <crivas@varicent.com>
2021-06-12 10:28:04 -0400

    updating api endpoint and renaming scss file

--- src/Main/index.js
+++ src/Main/index.js
@@ -1,9 +1,7 @@
 import { useEffect, useState } from 'react'
 import axios from 'axios'
 
-import './main.scss'
-
-const FOO = '44245687419d4dd2a3708a567af2b161'
+import './Main.scss'
 
 // fetching from API
 // using axios
@@ -19,23 +17,18 @@ const Main = () => {
   // 1 - a function
   // 2 - array of dependencies
   useEffect(() => {
-    axios
-      .get(
-        `https://newsapi.org/v2/everything?q=tesla&from=2021-05-11&sortBy=publishedAt&apiKey=${FOO}`
-      )
-      .then(({ data }) => {
-        setCatFactz(data.articles)
-      })
+    axios.get('https://cat-fact.herokuapp.com/facts').then(({ data }) => {
+      setCatFactz(data)
+    })
   }, [])
 
   return (
     <ul className="fact-card-container">
-      {catFactz.map(({ author, title, description }) => {
+      {catFactz.map(({ createdAt, text }) => {
         return (
           <li className="fact-card">
-            <p className="fact-card__author">{author}</p>
-            <p className="fact-card__title">{title}</p>
-            <p className="fact-card__description">{description}</p>
+            <p className="fact-card__date">{createdAt}</p>
+            <p className="fact-card__text">{text}</p>
           </li>
         )
       })}
